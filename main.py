7506254ba38e21be378d1248132960b343b37e4e
sinano1107 <tftf161107@icloud.com>
2022-09-26 15:05:45 +0900

    [get] start [get] updateを実装

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
-import numpy as np
+from agent import Agent
 
 app = FastAPI()
+agent = Agent()
+prevAction = 0
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/start")
+def start():
+    global agent
+    global prevAction
+    agent = Agent()
+    prevAction = int(agent.get_action())
+    return prevAction + 5
 
-@app.get("/vertices")
-def read_vertices():
-    return np.random.randint(3, 10 + 1)
+@app.get("/update")
+def update(reward: bool):
+    global prevAction
+    agent.update(prevAction, reward)
+    prevAction = int(agent.get_action())
+    return prevAction + 5
